8e2d2cf5130366bf60e9f9b66020053ddaf510e2
Fiquell Carter <fiquellcarter@gmail.com>
2026-01-02 00:08:39 +0800

    chore(tooling): configure Prettier Tailwind CSS plugin

--- prettier.config.ts
+++ prettier.config.ts
@@ -7,6 +7,8 @@ const config: Config = {
   plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
   importOrder: ["<BUILTIN_MODULES>", "", "<THIRD_PARTY_MODULES>", "", "^~/(.*)$", "", "^[.]"],
   importOrderTypeScriptVersion: "5.0.0",
+  tailwindStylesheet: "./app/app.css",
+  tailwindFunctions: ["clsx", "cva"],
 };
 
 export default config;
