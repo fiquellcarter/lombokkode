5a9ef20f4ba018c618364ee3b923e7dcbef15c1f
Fiquell Carter <fiquellcarter@gmail.com>
2026-01-01 20:41:06 +0800

    refactor(ui): export buttonVariants for reusability

--- app/components/vorent/button.tsx
+++ app/components/vorent/button.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "~/lib/utils";
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   [
     "inline-flex items-center justify-center gap-2",
     "shrink-0",
