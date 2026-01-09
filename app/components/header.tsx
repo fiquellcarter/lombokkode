59417f7e89623c5269bf4b9d09849f7ff931708b
Fiquell Carter <fiquellcarter@gmail.com>
2026-01-10 05:42:57 +0800

    feat(header): implement sticky behavior and scroll effect

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,5 +1,5 @@
 import { Menu, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
 import Logo from "~/assets/logo.png?format=webp";
@@ -7,26 +7,48 @@ import { Button } from "~/components/vorent/button";
 import { Heading } from "~/components/vorent/heading";
 import { TextLink } from "~/components/vorent/text";
 import { navigations } from "~/data/navigations";
+import { cn } from "~/lib/utils";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 20);
+    };
+
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="absolute inset-x-0 top-0 bg-background">
+    <header
+      className={cn(
+        "fixed inset-x-0 top-0 z-50 bg-background transition-[border] duration-200 ease-in-out",
+        isScrolled ? "border-b" : "border-transparent"
+      )}>
       <div className="container">
         <nav className="flex min-h-16 w-full items-center py-4">
           <div className="inline-flex w-1/2 items-center justify-start">
-            <div className="flex items-center gap-2 select-none">
-              <img src={Logo} alt="Logo" className="size-8" />
-              <Heading level={5}>
-                Lombok <span className="text-primary">Kode</span>
-              </Heading>
-            </div>
+            <Link to="/">
+              <div className="flex items-center gap-2 select-none">
+                <img src={Logo} alt="Logo" className="size-8" />
+                <Heading level={5}>
+                  Lombok <span className="text-primary">Kode</span>
+                </Heading>
+              </div>
+            </Link>
           </div>
           <div className="inline-flex w-1/2 items-center justify-end">
             <div className="hidden md:flex md:items-center md:gap-8">
               {navigations.map((navigation, index) => (
-                <TextLink key={index} render={<Link to={navigation.link} />}>
+                <TextLink
+                  key={index}
+                  render={<Link to={navigation.link} />}
+                  className="font-medium">
                   {navigation.title}
                 </TextLink>
               ))}
@@ -35,7 +57,7 @@ export default function Header() {
             <div className="md:hidden">
               <Button
                 variant="secondary"
-                size="icon-md"
+                size="icon-sm"
                 aria-label="Toggle Menu"
                 onClick={() => setIsOpen(!isOpen)}>
                 {isOpen ? <X /> : <Menu />}
