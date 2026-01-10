13d0427c74ca330034982cdade2e685da99666ca
Fiquell Carter <fiquellcarter@gmail.com>
2026-01-10 09:47:59 +0800

    chore(routes): disable ui route

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,8 @@
 import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
-const config: RouteConfig = [index("routes/home.tsx"), route("ui", "routes/ui.tsx")];
+const config: RouteConfig = [
+  index("routes/home.tsx"),
+  // route("ui", "routes/ui.tsx"),
+];
 
 export default config;
